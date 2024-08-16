ab0e3ab5dccce5ab7c6e8eb33a8d64a68eee0ca7
josemartinezrdev <josemartinezrdev@gmail.com>
2024-08-16 13:40:44 -0500

    feat: :tada: Initial Commit

---
+++ src/main/resources/ddl.sql
@@ -0,0 +1,127 @@
+-- Database
+
+DROP DATABASE IF EXISTS surveymanager;
+
+CREATE DATABASE surveymanager;
+
+USE surveymanager;
+
+-- Register and login
+
+CREATE TABLE users (
+    id INT(8) AUTO_INCREMENTAL,
+    enabled BOOLEAN,
+    username VARCHAR(12),
+    password VARCHAR(255),
+    CONSTRAINT pk_id_users PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+    id INT(8) AUTO_INCREMENTAL,
+    name VARCHAR(255),
+    CONSTRAINT pk_id_roles PRIMARY KEY (id)
+);
+
+CREATE TABLE users_roles (
+    user_id INT(8),
+    role_id INT(8),
+    CONSTRAINT users_roles_ids UNIQUE (role_id, user_id),
+    CONSTRAINT fk_users_roles_users FOREIGN KEY (user_id) REFERENCES users (id),
+    CONSTRAINT fk_users_roles_users FOREIGN KEY (role_id) REFERENCES roles (id)
+);
+
+-- Surveys
+
+CREATE TABLE surveys (
+    id INT(8) AUTO_INCREMENTAL,
+    created_at TIMESTAMP(6),
+    updated_at TIMESTAMP(6),
+    description VARCHAR(255),
+    name VARCHAR(255),
+
+    CONSTRAINT pk_id_surveys PRIMARY KEY (id)
+);
+
+CREATE TABLE chapter (
+    id INT(8) AUTO_INCREMENTAL,
+    created_at TIMESTAMP(6),
+    updated_at TIMESTAMP(6),
+    chapter_number VARCHAR(50),
+    chapter_title VARCHAR(50),
+
+    survey_id INT(8),
+
+    CONSTRAINT pk_id_chapter PRIMARY KEY (id),
+    CONSTRAINT fk_chapter_surveys FOREIGN KEY (survey_id) REFERENCES surveys
+);
+
+CREATE TABLE questions (
+    id INT(8) AUTO_INCREMENTAL,
+    created_at TIMESTAMP(6),
+    updated_at TIMESTAMP(6),
+    question_number VARCHAR(10),
+    response_type VARCHAR(20),
+    comment_question TEXT,
+    question_text TEXT,
+
+    chapter_id INT(8),
+
+    CONSTRAINT pk_id_question PRIMARY KEY (id),
+    CONSTRAINT fk_questions_chapter FOREIGN KEY (chapter_id) REFERENCES chapter
+);
+
+CREATE TABLE categories_catalog (
+    id INT(8) AUTO_INCREMENTAL,
+    created_at TIMESTAMP(6),
+    updated_at TIMESTAMP(6),
+    name VARCHAR(255),
+
+    CONSTRAINT pk_id_categories_catalog PRIMARY KEY (id)
+);
+
+CREATE TABLE response_options (
+    id INT(8) AUTO_INCREMENTAL,
+    created_at TIMESTAMP(6),
+    updated_at TIMESTAMP(6),
+    option_value INT(4),
+    typecomponenthtml VARCHAR(30),
+    comment_response TEXT,
+    option_text TEXT,
+
+    categorycatalog_id INT(8),
+    question_id INT(8),
+
+    -- Esta es la foranea que va a si misma.
+    parentresponse_id INT(8),
+
+    CONSTRAINT pk_id_reponse_options PRIMARY KEY (id),
+    CONSTRAINT fk_res_opts_cats_catg FOREIGN KEY (categorycatalog_id) REFERENCES categories_catalog (id),
+    CONSTRAINT fk_res_opts_questions FOREIGN KEY (question_id) REFERENCES questions (id),
+
+    CONSTRAINT fk_res_opts_res_opts FOREIGN KEY (parentresponse_id) REFERENCES response_options (id)
+);
+
+CREATE TABLE subresponse_options (
+    id INT(8) AUTO_INCREMENTAL,
+    subresponse_number INT(4),
+    created_at TIMESTAMP(6),
+    updated_at TIMESTAMP(6),
+    commponent_html VARCHAR(255),
+    subresponse_text VARCHAR(255),
+
+    responseoptions_id INT(8),
+
+    CONSTRAINT pk_id_response_options PRIMARY KEY (id),
+    CONSTRAINT fk_sub_res_opts FOREIGN KEY (responseoptions_id) REFERENCES response_options (id)
+);
+
+CREATE TABLE response_question (
+    id INT(8) AUTO_INCREMENTAL,
+    reponse_id INT(8),
+    subresponse_id INT(8),
+    responsetext VARCHAR(80),
+
+    CONSTRAINT pk_id_response_question PRIMARY KEY (id),
+    CONSTRAINT fk_res_quest_res_opts FOREIGN KEY (response_options) REFERENCES response_options (id),
+    CONSTRAINT fk_res_quest_sub_res_opts FOREIGN KEY (subresponse_id) REFERENCES subresponse_options (id)
+);
